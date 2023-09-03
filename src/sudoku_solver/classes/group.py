d90ba15ab343740e3e7dd6f58960b7c4705ddf1f
kingmoshe <liorporat11@gmail.com>
2023-09-03 18:02:13 +0300

    Add group code

--- src/sudoku_solver/classes/group.py
+++ src/sudoku_solver/classes/group.py
@@ -1,2 +1,17 @@
+from typing import List
+from sudoku_solver.classes.cell import Cell
+
+
 class Group:
-    pass
+    def __init__(self, cells: List[Cell]):
+        self.cells = cells
+
+    @property
+    def is_finished(self) -> bool:
+        return all(cell.is_finished for cell in self.cells)
+
+    def is_valid(self) -> bool:
+        """
+        Check if the group cells are currently valid
+        """
+        raise NotImplementedError()
