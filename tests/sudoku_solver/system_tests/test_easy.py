e606622b7b443f2c2ed510a2e7f02e9e13abb3f7
kingmoshe <liorporat11@gmail.com>
2024-06-12 15:07:03 +0300

    Improve test

--- tests/sudoku_solver/system_tests/test_easy.py
+++ tests/sudoku_solver/system_tests/test_easy.py
@@ -1,8 +1,12 @@
+import pytest
 from tests.sudoku_solver.utils.parser import get_board
 
 
-def test_easy():
-    assert True
-    easy_board = get_board('easy.txt')
+@pytest.mark.parametrize('board_name', [
+    'easy.txt',
+    'medium.txt',
+])
+def test_easy(board_name):
+    easy_board = get_board(board_name)
     easy_board.solve_while_can()
     assert easy_board.is_solved()
