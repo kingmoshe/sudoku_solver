466a2e99810e909cbdc38f500cdbba4736f9c93f
kingmoshe <liorporat11@gmail.com>
2024-06-14 18:24:01 +0300

    Add harder puzzles

--- tests/sudoku_solver/system_tests/test_special_board.py
+++ tests/sudoku_solver/system_tests/test_special_board.py
@@ -1,5 +1,6 @@
 import pytest
 from tests.sudoku_solver.utils.parser import get_board
+from sudoku_solver.classes.solver import solve_board_recursive
 from sudoku_solver.classes.boards.board_types.non_consecutive_board import add_non_consecutive_board_groups
 
 
@@ -8,12 +9,26 @@ from sudoku_solver.classes.boards.board_types.non_consecutive_board import add_n
     [
         "puzzle_1.txt",
         "puzzle_5.txt",
+        "puzzle_7.txt",
     ],
 )
 def test_non_consecutive(board_name):
     board = get_board(board_name, resource_path="../../resources/miracle_sudokus")
     add_non_consecutive_board_groups(board)
     board.solve_while_can()
-    #print()
-    #print(board)
+    print()
+    print(board)
+    assert board.is_solved()
+
+
+@pytest.mark.parametrize(
+    "board_name",
+    [
+        "puzzle_8.txt",
+    ],
+)
+def test_non_consecutive_complex(board_name):
+    board = get_board(board_name, resource_path="../../resources/miracle_sudokus")
+    add_non_consecutive_board_groups(board)
+    solve_board_recursive(board, amount_of_steps=1, best_guesses_size=3)
     assert board.is_solved()
